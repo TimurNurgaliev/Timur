8bf1df92bd8ac9f948109b4826f3e6a2526a3d9c
TimurNurgaliev <108256777+TimurNurgaliev@users.noreply.github.com>
2022-06-27 20:37:00 +0500

    Update 4-2.py

--- 4-2.py
+++ 4-2.py
@@ -1,6 +1,18 @@
 class ReversedList:
   def __init__(self, lst):
-    self.l = list(reversed(lst))
+    self.list = list(reversed(lst))
      
   def __str__(self):
-    return str(self.l)
+    return str(self.list)
+    
+  def __getitem__(self, i):
+    return self.list[i]
+    
+  def __len__(self):
+    return len(self.list)
+  
+  
+
+rl = ReversedList([10, 20, 30])
+for i in range(len(rl)):
+  print(rl[i])
